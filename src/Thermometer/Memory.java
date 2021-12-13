b2f477c1e97043efd3a7dec07c35875779d0274a
finkbran <56618815+finkbran@users.noreply.github.com>
2021-12-13 17:56:47 -0600

    Update Memory.java

--- src/Thermometer/Memory.java
+++ src/Thermometer/Memory.java
@@ -1,4 +1,30 @@
 package Thermometer;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
-class Memory{
+public class memory {
+    ArrayList<Temperature> measuredTemperatures;
+    ArrayList<Integer> temps = new ArrayList<Temperature>();
+    getTemp(ArrayList<Temperature> storedTemperatures);
+
+    public ArrayList<Temperature> getMeasuredTemperatures() {
+        return measuredTemperatures;
+    }
+
+    public memory (ArrayList<Temperature> storedTemperatures) { //Call temp array from temperature class (Constructors?)\
+        ArrayList<Temperature> temps;
+        this.measuredTemperatures = new ArrayList<Temperature>();
+        for(Temperature t : temps) {
+            storedTemperatures.add(t);
+        }
+
+    }
+
+    public void writeToStorage(PrintStream p) {
+        for (Temperature t : measuredTemperatures) {
+            p.println(t.getAverageTemperature() + " " + t.getTimeStamp());
+        }
+    }
+
+
 }
