b2d6ef1907c24db61d52e405710e238e18da1524
xw27 <xw27@illinois.edu>
2021-12-13 19:04:02 -0600

    add a few methods in power class

--- src/Thermometer/Power.java
+++ src/Thermometer/Power.java
@@ -1,4 +1,32 @@
 package Thermometer;
 
 class Power{
+    static boolean power;
+    public static powerOn(){
+
+        if (power == true){
+
+            selfTest();
+        }
+    }
+
+
+    //test button, power, temperature, display, memory, sound, settings, erro/log
+    // if all the class passed, then good to go.
+    public boolean selfTest(){
+        boolean passed;
+
+        if (passed = true){
+            return true;
+
+        }else {
+            return false;
+        }
+
+    }
+    // shutdown all the activity
+    public boolean powerOff(){
+        return power = false;
+
+    }
 }
